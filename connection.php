dda0b41b0be45d412ae7656a34f6311620fae4cc
Ayush Singh <91191012+ayush2883@users.noreply.github.com>
2023-09-03 13:17:13 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php
 
-    $database= new mysqli("localhost","root","","databases");
+    $database= new mysqli("localhost","ayush","1234","databases");
     if ($database->connect_error){
         die("Connection failed:  ".$database->connect_error);
     }
